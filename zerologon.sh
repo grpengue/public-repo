6100198df2fec69f977c7a7103e442668bdc519e
grpengue <79980538+grpengue@users.noreply.github.com>
2022-05-31 23:22:34 -0300

    Delete zerologon.sh

--- zerologon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo -ne "\e]2;Metasploit\a"			#change terminal window title to be grabbed by auto script
-rm -rf /var/www/html/upload/*			#empty files from previous demos
-rm -f /home/aptuser/*				#empty files from previous demos
-service apache2 start				#start HTTP server
-service vsftpd start				#start FTP server
-/DemoTools/aptdemo/sendemail.sh			#send phishing email
